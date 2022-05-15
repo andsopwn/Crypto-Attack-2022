cc92c668475c80f96b28b72b2b1018b3cd252520
louxsoen <iscisx76@gmail.com>
2022-05-15 22:15:28 +0900

    mlp about

---
+++ MLP/zerocross.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a = -1.6702 * pow(2, -3);
+    float b = -1.5906 * pow(2,  5);
+
+    printf("%u\n", (int)b << 5);
+    printf("%f\n", 1.1255 * pow(2, -7));
+}
